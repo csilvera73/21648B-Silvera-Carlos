3ef7ec1e490514530e2cd6ff99a47b1628b52681
Carlos <your-email-address>
2023-09-24 15:00:58 -0300

    Se agregó la funcionalidad de editar con su vista correspondiente

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,11 +1,15 @@
 const {Router} = require("express")
-const { getAllPosts, formCreateNewPost, createPost } = require("../src/controller/posts.controllers")
+const { getAllPosts, formCreateNewPost, formUpdatePost, createPost, updatePost } = require("../src/controller/posts.controllers")
 
 
 const router = Router()
 
 router.get("/", getAllPosts)
 router.get("/create", formCreateNewPost)
+router.get("/update/:id", formUpdatePost)  // ruta con parametro dinamico
+
+
 router.post("/", createPost)
+router.post("/update", updatePost)
 
 module.exports = router
--- src/controller/posts.controllers.js
+++ src/controller/posts.controllers.js
@@ -11,12 +11,34 @@ const formCreateNewPost = async (req, res) => {
     res.render("new-post")
 }
 
+const formUpdatePost = async (req, res)=> {
+     const postId =req.params.id  // capturo el parametro dinamico
+
+     const post = await PostModel.findByPk(postId) //Busqueda por primary key
+
+     //Si no existe el Id, redirecciono al /posts
+     if (!post) {
+        return res.redirect("/posts")
+     }
+
+    res.render("update-post", {post})
+}
+
 const createPost = async (req,res) =>{
     const {title, content, imglink } = req.body
     await PostModel.create({title, content, imglink } )
    /*  res.send ("Post created successfully") */
    res.redirect("/posts")
+}
+const updatePost = async (req,res) =>{
+    const {id, title, content, imglink } = req.body
 
+    const post = await PostModel.findByPk(id)
+
+   await post.update({title, content, imglink })
+    
+   res.redirect("/posts")
 }
 
-module.exports = {getAllPosts, formCreateNewPost, createPost}
+
+module.exports = {getAllPosts, formCreateNewPost, createPost, formUpdatePost, updatePost}
